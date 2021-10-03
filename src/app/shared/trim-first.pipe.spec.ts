2ac92abcf81a7c074d34fbca13de7c513b31d958
prasanna kumar <aprasannakumarcloud85@gmail.com>
2021-10-03 18:21:13 -0400

    remove unit testing files

--- src/app/shared/trim-first.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TrimFirstPipe } from './trim-first.pipe';
-
-describe('TrimFirstPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TrimFirstPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
